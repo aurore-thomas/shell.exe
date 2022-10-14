db2775e4bf1cd01a9e3d6024c49b53566414250b
aurore-thomas <aurore.thomas@laplateforme.io>
2022-10-14 21:57:17 +0200

    Ajout job Pour aller plus loin

---
+++ Pour_aller_plus_loin/Connexion_alcasar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Username=$1
+Mdp=$2
+
+#Enregistrer code html dans un fichier alcasar.html
+curl -o /home/aurore/Shell.exe/Pour_aller_plus_loin/alcasar.html https://alcasar.laplateforme.io/intercept.php
+
+
+
+#chmod u=rwx /home/aurore/Shell.exe/Pour_aller_plus_loin/alcasar.html
+
+#Modifier alcasar.html
+#sed 's/username/$Username/' alcasar.html /home/aurore/Shell.exe/Pour_aller_plus_loin
+#sed 's/password/$Mdp/2' alcasar.html /home/aurore/Shell.exe/Pour_aller_plus_loin
+#sed 's/button/Authentication/' alcasar.html /home/aurore/Shell.exe/Pour_aller_plus_loin
+
+
+
+#Ouvrir sur navigateur
+open ./
+
+
+
+
+
